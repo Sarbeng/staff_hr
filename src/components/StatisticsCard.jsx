b1e6e5a73f639db35dd907a449bf3bbc719cd340
Sarbeng <sarbengbaafi@gmail.com>
2023-08-14 11:21:17 +0000

    dahsboard statistics css changed for responsiveness

--- src/components/StatisticsCard.jsx
+++ src/components/StatisticsCard.jsx
@@ -5,7 +5,7 @@ export default function StatisticsCard (props) {
         <>
             <section className="bg-white w-full  shadow-sm p-5 border border-primary-border rounded-lg ">
               <div className="flex flex-col place-items-center items-center gap-8">
-                <h3 className="text-base h-14 text-center">
+                <h3 className="text-base h-8 text-center">
                   {props.heading}
                 </h3>
                 <p className="text-3xl">{props.number}</p>
--- src/pages/dashboard/components/dashboard_statistics.jsx
+++ src/pages/dashboard/components/dashboard_statistics.jsx
@@ -3,15 +3,15 @@ import StatisticsCard from "../../../components/StatisticsCard";
 export default function DashboardStatistics () {
     const statisticsData = [
         {heading:"Total Promotion", number:3},
-        {heading:"Approved Promotion", number:3},
-        {heading:"Disapproved Promotion", number:3},
+        {heading:"Approved Promotion", number:0},
+        {heading:"Disapproved Promotion", number:0},
     ]
     return (
         <>
             <section className="flex md:flex-row flex-col gap-8 justify-between">
                 {
                     statisticsData.map((statistics)=>{
-                        return <div key={statistics.heading} className="w-1/3">
+                        return <div key={statistics.heading} className="md:w-1/3">
                         <StatisticsCard heading={statistics.heading} number={statistics.number}/>
                         </div>
                     })
