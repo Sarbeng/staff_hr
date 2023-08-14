cc53b3cf6abbd214c9cee2909483c4f0c54459cf
Sarbeng <sarbengbaafi@gmail.com>
2023-08-14 14:51:31 +0000

    recent promotion started

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import DashboardStatistics from "./components/dashboard_statistics";
+import RecentPromotions from "./components/recent_promotions";
 
 export default function Dashboard () {
     return (
         <>
             <DashboardStatistics/>
+            <RecentPromotions/>
         </>
     );
 }
