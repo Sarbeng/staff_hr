37717ea5529176e82b2440f6ed9e1232887a5225
Sarbeng <sarbengbaafi@gmail.com>
2023-08-15 16:37:36 +0000

    search form for position assignment added

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -6,7 +6,8 @@ export default function SelectInput(props) {
         <label className="capitalize" htmlFor="">
 
         </label>
-            <select name={props.name} className={`mt-2 mb-2 h-12 border border-primary-border rounded-lg w-full py-2 px-3 text-primary-main focus:outline-none  focus:border-primary-focused focus:ring-1 shadow-sm focus:ring-primary-focused  bg-white`}>
+            <select name={props.name} className={` h-12 border border-primary-border rounded-lg w-full py-2 px-3 text-primary-main focus:outline-none  focus:border-primary-focused focus:ring-1 shadow-sm focus:ring-primary-focused  bg-white`}>
+                <option selected disabled>Select {props?.disabledOption}</option>
                 {option}
             </select>
         </>
--- src/pages/Hr/positon_assignment/components/searchArea.jsx
+++ src/pages/Hr/positon_assignment/components/searchArea.jsx
@@ -5,6 +5,9 @@ export default function SearchArea () {
     const area = [
         {id:1,text:"Accra Office"},{id:2,text:"Accra Guest House"}
     ]
+    const area2 = [
+        {id:1,text:"Accra Office"},{id:2,text:"Accra Guest House"}
+    ]
 
     const handleClick = (e) => {
         e.preventDefault()
@@ -13,10 +16,10 @@ export default function SearchArea () {
     return (
         <>
             <section id="searchSection">
-                <form className="flex gap-4 w-full bg-primary-surface h-24 rounded-lg items-center px-4 shadow-sm">
-                <SelectInput name={"select_department"} options={area}/>
-                <SelectInput name={"select_department"} options={area}/>
-                <button className="bg-primary-main text-white px-6 py-2 rounded-lg h-12">Search</button>
+                <form className="flex gap-4 w-full bg-primary-surface h-24 rounded-lg items-center justify-center px-4 shadow-sm">
+                <SelectInput name={"select_department"} options={area} disabledOption={"Department"}/>
+                <SelectInput name={"select_department"} options={area2} disabledOption={"Position"}/>
+                <button type="submit" onClick={handleClick} className="bg-primary-main text-white px-6 py-2 rounded-lg h-12">Search</button>
                 </form>
             </section>
         </>
