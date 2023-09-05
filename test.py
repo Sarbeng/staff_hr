5d0c60c62f2c01902bc31999426c2d3123819d99
Sarbeng <sarbengbaafi@gmail.com>
2023-09-05 09:41:04 +0000

    personal_details

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-# Fix the code
-part1 = input("Please type in a name: ")
-part2 = input("Please type in a year: ")
-print(part1+" is a valiant knight, born in the year " + part2 + "One morning Mary woke up to an awful racket: a dragon was approaching the village. Only Mary could save the village's residents.")
 
+numInput = (input("Please type a number: "))
+newNum = int(numInput) * 5
+print(f"{numInput} times 5 is {newNum}")
