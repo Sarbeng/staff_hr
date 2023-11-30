448a98ee0bd3fb088434c3a9c45fdee43908d164
Sarbeng <sarbengbaafi@gmail.com>
2023-11-30 15:34:04 +0000

    jobs and user data displayed on promotion page 1

--- src/pages/staff/promotion/pages/PromotionPage1.jsx
+++ src/pages/staff/promotion/pages/PromotionPage1.jsx
@@ -2,14 +2,14 @@
 
 
 import TextInput from "../../../../components/TextInput";
-import { useFormik } from "formik";
-import * as Yup from "yup";
 import Button from "../../../../components/Button";
 import { useNavigate } from "react-router-dom"
 import { MdOutlineChevronRight } from "react-icons/md";
 import axios from "../../../../api/axios";
 import { useAuthUser } from "react-auth-kit";
 import { useEffect, useState } from "react";
+import Select from "react-select";
+
 
 export default function PromotionPage1() {
     const navigate = useNavigate()
@@ -21,6 +21,16 @@ export default function PromotionPage1() {
     const [promotionData,setPromotionData] = useState(null)
     // getting the select options from data
     const [selectOptions, setSelectOptions] = useState(null)
+
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' },
+      ];
+
+      const [selectedOption, setSelectedOption] = useState(null);
+
+      
 
     //getting token
     const auth  = useAuthUser();
@@ -37,9 +47,9 @@ export default function PromotionPage1() {
         })
 
         if (response) {
-            console.log(response.data.user_data[0])
+            console.log(response.data.job_ranks)
             setPromotionData(response.data.user_data[0])
-            setSelectOptions(response.data.job_ranks[0])
+            setSelectOptions(response.data.job_ranks)
             console.log(promotionData)
             console.log(selectOptions)
 
@@ -50,31 +60,7 @@ export default function PromotionPage1() {
         getPromotionData()
     },[])
 
-    const formik = useFormik({
-        initialValues: {
-            aspiring_rank: "",
-            department: "",
-            directorate: "",
-            email: "",
-            phone: "",
-            present_position: "",
-            effective_date: ""
-        },
-        validationSchema: Yup.object({
-            aspiring_rank: Yup.string().required("Position must be selected"),
-            department: Yup.string().required("Department should not be empty"),
-            directorate: Yup.string().required("Directorate cannot be empty"),
-            email: Yup.string().required(),
-            phone: Yup.number().required(),
-            present_position: Yup.string().required("Present position is a  required field"),
-            effective_date: Yup.date().required("Effective date cannot be empty")
-        }),
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
-            navigate('/dashboard/promotionPage2')
-
-        }
-    })
+   
     return (
         <>
             <div className="flex flex-col gap-8 pb-8">
@@ -83,141 +69,127 @@ export default function PromotionPage1() {
                         <div className="w-full md:w-fit  bg-primary-surface py-4  px-3 rounded-lg flex justify-center items-center gap-3">
 
                             <div className="flex gap-2 items-center">
-                                <p className="text-primary-main capitalize">Application for Promotion</p>
+                                <p className="text-primary-main capitalize text-sm w-full">Application for Promotion</p>
                                 <hr className="inline-block h-full min-h-[1.5em] w-0.5  bg-neutral-300 opacity-100 dark:opacity-50" />
-                                <p className="bg-primary-main text-white  text-sm py-2 px-8 rounded-full">Page 1 of 6</p>
+                                <p className="bg-primary-main text-white  text-sm py-1 px-8 rounded-full w-full">Page 1 of 6</p>
                             </div>
 
                         </div>
                     </section>
-                    <form onSubmit={formik.handleSubmit}>
+                    <form >
                         <section id="inputs" className="flex flex-col gap-4">
                             <div id="inputGroup">
                                 <label className="capitalize" htmlFor="email">
                                     Promotion to the rank of
                                 </label>
-                                <select className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.aspiring_rank && formik.errors.aspiring_rank ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`}
+                                {/* Tried a select dropdown select here */}
+                                {/* <Select
+                                className="  mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main  border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused  bg-white"
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={selectOptions}
+      /> */}
+                                <select 
+                              className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main  border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused  bg-white`}
                                     name="aspiring_rank"
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    value={formik.values.aspiring_rank}>
+                                   
+                                    
+                                    >
                                     <option selected disabled>Select Rank</option>
-                                    <option>Administrator</option>
+                                    {selectOptions?.map((options) => {
+                                        return <option>{options.title}</option>
+                                    })}
                                     <option>Administrator 2</option>
                                 </select>
-                                {formik.touched.aspiring_rank && formik.errors.aspiring_rank ? (
-                                    <div className="text-red-600">{formik.errors.aspiring_rank}</div>
-                                ) : null}
+                            
                             </div>
                             <div>
                                 <TextInput
-                                    inputStyle={formik.touched.department && formik.errors.department ? ("error") : "default"}
+                                    inputStyle={"default"}
                                     name="department"
                                     type="text"
                                     label="Department"
-                                    handleBlur={formik.handleBlur}
-                                    handleChange={formik.handleChange}
-                                    value={formik.values.department} />
-                                {formik.touched.department && formik.errors.department ? (
-                                    <div className="text-red-600">{formik.errors.department}</div>
-                                ) : null}
+                
+                                    value={promotionData?.directorate} 
+                                    />
                             </div>
                             <div>
                                 <TextInput
-                                    inputStyle={formik.touched.directorate && formik.errors.directorate ? ("error") : "default"}
+                                      inputStyle={"default"}
                                     label="College/ Directorate"
                                     name="directorate"
                                     type="text"
-                                    handleBlur={formik.handleBlur}
-                                    handleChange={formik.handleChange}
-                                    value={formik.values.directorate}
+                                
+                                    value={promotionData?.directorate} 
                                 />
-                                {formik.touched.directorate && formik.errors.directorate ? (
-                                    <div className="text-red-600">{formik.errors.directorate}</div>
-                                ) : null}
+        
                             </div>
 
-                            <div className="flex gap-4 w-full">
-                                <div id="inputGroup" className="flex flex-col w-1/2" >
+                            <div className="flex flex-col md:flex-row gap-4 w-full">
+                                <div id="inputGroup" className="flex flex-col  md:w-1/2" >
                                     <label className="capitalize" htmlFor="email">
                                         E-mail Address
                                     </label>
                                     <input
-                                        className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.email && formik.errors.email ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`}
+                                        className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main  border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused  bg-white`}
                                         name="email"
                                         type="email"
-                                        value={formik.values.email}
-                                        onBlur={formik.handleBlur}
-                                        onChange={formik.handleChange}
+                                        value={promotionData?.ucc_mail}
+                                        placeholder="Not set"
 
                                     />
-                                    {formik.touched.email && formik.errors.email ? (
-                                        <div className="text-red-600">{formik.errors.email}</div>
-                                    ) : null}
+                                  
                                 </div>
-                                <div id="inputGroup" className="flex flex-col w-1/2" >
+                                <div id="inputGroup" className="flex flex-col md:w-1/2" >
                                     <label className="capitalize" htmlFor="email">
                                         Telephone
                                     </label>
                                     <input
-                                        className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.phone && formik.errors.phone ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`}
+                                        className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main  border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused  bg-white`}
                                         name="phone"
-                                        type="text"
-                                        value={formik.values.phone}
-                                        onBlur={formik.handleBlur}
-                                        onChange={formik.handleChange}
-
+                                        type="number"
+                                        value={promotionData?.phone}
+                                        placeholder="Not set"
 
                                     />
-                                    {formik.touched.phone && formik.errors.phone ? (
-                                        <div className="text-red-600">{formik.errors.phone}</div>
-                                    ) : null}
+                                  
                                 </div>
-                            </div>
-
-                            <div className="flex gap-4 w-full">
-                                <div id="inputGroup" className="flex flex-col w-1/2" >
+                               
+</div>
+                            <div className="flex flex-col md:flex-row  gap-4 w-full">
+                                <div id="inputGroup" className="flex flex-col md:w-1/2" >
                                     <label className="capitalize" htmlFor="email">
                                         Present Position held (If any)
                                     </label>
                                     <input
-                                        className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.present_position && formik.errors.present_position ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`}
+                                        className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main  border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused  bg-white`}
                                         name="present_position"
                                         type="text"
-                                        value={formik.values.present_position}
-                                        onBlur={formik.handleBlur}
-                                        onChange={formik.handleChange}
+                                        value={promotionData?.present_rank}
+                                     
 
                                     />
-                                    {formik.touched.present_position && formik.errors.present_position ? (
-                                        <div className="text-red-600">{formik.errors.present_position}</div>
-                                    ) : null}
+                                  
                                 </div>
-                                <div id="inputGroup" className="flex flex-col w-1/2" >
+                                <div id="inputGroup" className="flex flex-col md:w-1/2" >
                                     <label className="capitalize" htmlFor="email">
                                         Effective Date
                                     </label>
                                     <input
-                                        className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main ${formik.touched.effective_date && formik.errors.effective_date ? "border-red-500 focus-within:outline-none  focus-within:border-red-500 focus-within:ring-1 shadow-sm focus-within:ring-red-500" : "border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused"}  bg-white`}
+                                          className={` mt-2 mb-2 h-12 border text-sm border-primary-border rounded-lg w-full py-2 px-3 text-primary-main  border-primary-border focus-within:outline-none  focus-within:border-primary-focused focus-within:ring-1 shadow-sm focus-within:ring-primary-focused  bg-white`}
                                         name="effective_date"
                                         type="date"
-                                        value={formik.values.effective_date}
-                                        onBlur={formik.handleBlur}
-                                        onChange={formik.handleChange}
-
+                                        value={promotionData?.date_of_rank}
+                                       
                                     />
-                                    {formik.touched.effective_date && formik.errors.effective_date ? (
-                                        <div className="text-red-600">{formik.errors.effective_date}</div>
-                                    ) : null}
+                                   
                                 </div>
                             </div>
 
                         </section>
-                        <section id="buttonSection" className="flex w-full gap-4 items-end justify-end">
-                            <div className="w-1/2">
-                                {/* <Button buttonStyle="bg-white border border-primary-border shadow text-primary-main w-full" ><MdOutlineChevronLeft/> Back</Button> */}
-                            </div>
-                            <div className="w-1/2 ">
+                        <section id="buttonSection" className="flex w-full gap-4 mb-2 justify-center items-center md:items-end md:justify-end">
+                           
+                            <div className="md:w-1/2 w-full">
                                 <Button buttonStyle="bg-primary-main text-white w-full">Save & Continue <MdOutlineChevronRight /></Button>
 
                             </div>
